32cad55b259dedf06d569edec85cb85a72c7b8f1
Sergey Nazarov <ric19902002@icloud.com>
2025-08-26 21:14:57 +0300

    Update Hospital.java

--- src/main/java/practice/hospital/Hospital.java
+++ src/main/java/practice/hospital/Hospital.java
@@ -3,8 +3,8 @@ package practice.hospital;
 public class Hospital {
     private static final float MIN_DIAPAZON = 32.00F;
     private static final float MAX_DIAPAZON = 40.00F;
-    private static final float MAX_TEMPERATURE = 36.2F;
-    private static final float MIN_TEMPERATURE = 36.9F;
+    private static final float MAX_HEALTHY_TEMP = 36.2F;
+    private static final float MIN_HEALTHY_TEMP = 36.9F;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
         float[] allTemperature = new float[patientsCount];
@@ -16,36 +16,34 @@ public class Hospital {
             allTemperature[i] = result2;
         }
         return allTemperature;
-
     }
 
     public static String getReport(float[] temperatureData) {
         float sumTemperature = 0.0F;
-        for (int i = 0; i < temperatureData.length; i++) {
-
-            sumTemperature += temperatureData[i];
-
+        for (float temp : temperatureData) {
+            sumTemperature += temp;
         }
         float averageTemperature = sumTemperature / temperatureData.length;
         averageTemperature = averageTemperature * 100;
         int result = Math.round(averageTemperature);
-        float result2 = (float) result / 100;
+        float roundedAverage = (float) result / 100;
 
         int numberOfHealthy = 0;
-        for (int i = 0; i < temperatureData.length; i++) {
-            if (temperatureData[i] <= MIN_TEMPERATURE && temperatureData[i] >= MAX_TEMPERATURE) {
+        for (float temp : temperatureData) {
+            if (temp <= MIN_HEALTHY_TEMP && temp >= MAX_HEALTHY_TEMP) {
                 numberOfHealthy++;
             }
         }
+
         String listTemperature = "";
-        for (int i = 0; i < temperatureData.length; i++) {
-            listTemperature = listTemperature.concat(temperatureData[i] + " ");
+        for (float temp : temperatureData) {
+            listTemperature = listTemperature.concat(temp + " ");
         }
-        String report =
-                "Температуры пациентов: " + listTemperature.trim() +
-                        "\nСредняя температура: " + result2 +
-                        "\nКоличество здоровых: " + numberOfHealthy;
-        return report;
+
+        return "Patient temperatures: " + listTemperature.trim() +
+                "\nAverage temperature: " + roundedAverage +
+                "\nNumber of healthy patients: " + numberOfHealthy;
     }
 }
+
 
