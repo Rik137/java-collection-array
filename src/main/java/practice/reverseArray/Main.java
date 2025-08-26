7e690956df4d632bd0ceb9683cc72f5bc2971838
Sergey Nazarov <ric19902002@icloud.com>
2025-08-26 21:13:47 +0300

    Update Main.java

--- src/main/java/practice/reverseArray/Main.java
+++ src/main/java/practice/reverseArray/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String line = "Каждый охотник желает знать, где сидит фазан";
+        String line = "Every hunter wants to know where the pheasant sits";
         String[] lines = line.split(" ");
         ReverseArray.reverse(lines);
         System.out.println(Arrays.toString(lines));
